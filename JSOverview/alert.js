852c6d82a7c8bc5b3a8912d71bc42f3d95506cfa
bichthuy <bichthuydangthi198@gmail.com>
2022-11-09 14:22:21 +0700

    add alert js file

---
+++ JSOverview/alert.js
@@ -0,0 +1,15 @@
+function showAlert() {
+    alert("Hi");
+}
+
+function writeAlert() {
+    document.write('hi');
+}
+
+function inConsole() {
+    console.log('Hi');
+}
+
+function changeName() {
+    document.getElementById('btn').innerHTML='Hi';
+}
