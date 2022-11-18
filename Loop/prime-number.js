4f81c2fbc32ce91c2b43fbe4adc37d0b0918ce5a
bichthuy <bichthuydangthi198@gmail.com>
2022-11-18 16:45:48 +0700

    display prime number

---
+++ Loop/prime-number.js
@@ -0,0 +1,36 @@
+let number = 20;
+let count = 1;
+let N = 3;
+let range = 0;
+let primeArr = [2];
+// function tính range
+function rangeN(num) {
+    if (num % 2 != 0) {
+        num++;
+    }
+    return range = num / 2;
+}
+// ktra số nguyên tố
+function isPrime(num, range) {
+    range = rangeN(num);
+    let count = 0;
+    for (let i = 2; i <= range; i++) {
+        if (num % i == 0) {
+            count++;
+        }
+    }
+    if (count == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+// check count và in ra mảng 
+while (count < number) {
+    if (isPrime(N, range)) {
+        count++;
+        primeArr.push(N);
+    }
+    N++;
+}
+console.log(primeArr);
