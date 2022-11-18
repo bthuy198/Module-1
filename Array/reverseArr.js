0dbfdcaa27274c77e78350c65be31c57583c1e1c
bichthuy <bichthuydangthi198@gmail.com>
2022-11-18 08:36:37 +0700

    reverse array

---
+++ Array/reverseArr.js
@@ -0,0 +1,8 @@
+let a = [1, 0, 4, 7, 10, 11, 20,24,3, 19];
+let first = 0;
+let last = a.length - 1;
+let reverse_a = [];
+for (last; last >= first; last--) {
+    reverse_a.push(a[last]);
+}
+console.log(reverse_a);
