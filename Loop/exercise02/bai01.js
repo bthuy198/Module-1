570f8ca09fede67665330ef80cd7b41962525911
bichthuy <bichthuydangthi198@gmail.com>
2022-12-05 11:12:01 +0700

    exercise02

---
+++ Loop/exercise02/bai01.js
@@ -0,0 +1,8 @@
+let i1 = 0; i2 = 1; iN = 0;
+let i = 1;
+for (i; i <= 10; i++) {
+    console.log(i1);
+    iN = i1 + i2;
+    i1 = i2;
+    i2 = iN;
+}
---
+++ Loop/exercise02/bai02.js
@@ -0,0 +1,6 @@
+let number = 5;
+let factorial = 1;
+for(let i = 1; i <= number; i++){
+    factorial *= i;
+}
+console.log(factorial);
